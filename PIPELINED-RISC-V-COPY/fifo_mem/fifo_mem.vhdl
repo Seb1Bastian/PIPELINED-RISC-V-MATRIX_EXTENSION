60565dd67ce1fa02cc2f9c7cbcee224b5c85d106
Seb1Bastian <95043164+Seb1Bastian@users.noreply.github.com>
2022-01-07 18:01:36 +0100

    Ein tatsächlicher fifo_mem mit zwei clk eine zum lesen, die andere zum schreiben

--- PIPELINED-RISC-V-COPY/fifo_mem/fifo_mem.vhdl
+++ PIPELINED-RISC-V-COPY/fifo_mem/fifo_mem.vhdl
@@ -4,20 +4,19 @@ use ieee.numeric_std.all;
 use work.fifo_mem_pack.ALL; 
 
 entity fifo_mem is
-    generic(size : integer range 2 to 255 := 3); -- the specificed number of rising edges of the ended signal turns to one
+    generic(size : integer range 1 to 255 := 3); -- the specificed number of rising edges of the ended signal turns to one
     port(
-        --inputs
-        clk         : in std_logic;
-        initialize  : in std_logic;
-        reset       : in std_logic;  -- synchron Reset
-        pos_x       : in integer;
-        pos_y       : in integer;
-        write_en    : in std_logic;
-
-        data_in        : in std_logic_vector(7 downto 0);
+        clk_1       : in std_logic;
+        clk_2       : in std_logic;
+        reset       : in std_logic;                     -- synchron Reset
+        data_in     : in std_logic_vector(7 downto 0);
+        write_data  : in std_logic;
+        read_data   : in std_logic;
 
         --outputs
-        data_out    : out BYTE_VECTOR(size-1 downto 0)  -- is 1 when it is finished counting else 0
+        data_out    : out std_logic_vector(7 downto 0);  -- is 1 when it is finished counting else 0
+        can_write   : out std_logic;
+        can_read    : out std_logic
     );
 end fifo_mem;
 
@@ -25,40 +24,50 @@ end fifo_mem;
 architecture rtl of fifo_mem is
 
     signal vector : BYTE_VECTOR(size-1 downto 0);
-    Signal grid : BYTE_GRID( size-1 downto 0, size-1 downto 0); -- (0,0) is the upper left corner. (size-1, size-1) is the lower right corner.
+    signal written   : std_logic_vector(size-1 downto 0);
+    --signal adr_write : integer range 0 to size :=0;
+    --signal adr_read  : integer range 0 to size :=0;
     
     begin
 
-        process(clk)
+        can_writing : entity work.unary_nand(rtl) port map( inp => written, outp => can_write);
+        can_reading : entity work.unary_or(rtl) port map( inp => written, outp => can_read);
+
+
+        process(clk_1)  
+        variable v_adr_write : integer range 0 to size := 0;
         begin
-            if(rising_edge(clk)) then
-                if(reset = '1') then
-                    for i in 0 to size-1 loop                   --everthing to zero
-                        for j in 0 to size-1 loop
-                            grid(i,j) <= x"00";
-                        end loop ;     
-                    end loop ;
-                elsif initialize = '1' then                     --maybe not important (can this whole else if away?)
-                    for i in 1 to size-1 loop
-                        for j in size-1 downto size-1-i loop
-                            grid(i,j) <= x"00";
-                        end loop ;     
-                    end loop ;
-                elsif write_en = '1' then
-                    grid(pos_x,pos_y) <= data_in;
-                else
-                    for i in size-1 downto 0 loop               --shift
-                        vector(i)<= grid(i,size-1);
-                    end loop ;
-                    for i in size-1 downto 0 loop
-                        for j in size-1 downto 1 loop
-                            grid(i,j) <= grid(i,j-1);
-                        end loop ;     
-                    end loop ;
+            if(rising_edge(clk_1)) then
+                if(write_data = '1' and v_adr_write < size and written(v_adr_write) = '0') then        --Do you have enough time to read out the cell by looking ahead only one cell ahead (worst case) (maybe looking two cell ahead might be a good idea?)
+                    vector(v_adr_write) <= data_in;
+                    --adr_write <= adr_write + 1;
+                    written(v_adr_write) <= '1';
+                    v_adr_write := v_adr_write + 1;
+                elsif write_data = '1' and v_adr_write = (size) and written(0) = '0' then
+                    vector(0) <= data_in;
+                    --adr_write <= 0;
+                    written(0) <= '1';
+                    v_adr_write := 0;
                 end if;
-                    
+            end if;
+        end process;
 
 
+        process(clk_2)
+        variable v_adr_read : integer range 0 to size := 0;
+        begin
+            if(rising_edge(clk_2)) then
+                if(read_data = '1' and v_adr_read < size and written(v_adr_read) = '1') then
+                    data_out <= vector(v_adr_read);
+                    v_adr_read := v_adr_read + 1;
+                    written(v_adr_read) <= '0'
+                    --adr_read <= adr_read + 1;
+                elsif read_data = '1' and v_adr_read = (size) and  written(0) = '1' then
+                    data_out <= vector(0);
+                    v_adr_read := 0;
+                    written(0) <= '1';
+                    --adr_read <= 0;
+                end if;
             end if;
         end process;
 
